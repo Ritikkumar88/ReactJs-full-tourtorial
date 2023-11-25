783c233e127787a5c6a288cb3616b94ce6a6e1b1
Ritikshroff <109219868+Ritikshroff@users.noreply.github.com>
2023-11-25 18:58:57 +0530

    Context api Discussion and a Project

---
+++ 08_Mini_Context_project/src/components/Login.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState , useContext} from "react";
+import UserContext from "../context/UserContext";
+
+
+function Login(){
+
+    const [userName , setusername] = useState('')
+    const [password , Setpassword] = useState('')
+
+    const {setUser} = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+            e.preventDefault()
+            setUser({userName  , password})
+    }
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text" 
+            value={userName}
+            onChange={(e) => {
+                setusername(e.target.value)
+            }}
+            placeholder="UserName" />
+
+            <input type="password" 
+            value={password}
+            onChange={(e) => {
+                Setpassword(e.target.value)
+            }}
+
+            placeholder="Password" />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login;
