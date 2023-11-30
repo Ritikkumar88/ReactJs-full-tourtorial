c28db9ce3af030d268cd1f5e5b7025e3b0a66445
Ritikshroff <109219868+Ritikshroff@users.noreply.github.com>
2023-11-30 12:31:59 +0530

    password genrator optimization

--- 05_password_creating_project1_1/src/App.jsx
+++ 05_password_creating_project1_1/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback , useEffect, useRef} from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -13,14 +13,17 @@ function App() {
   // using useref Hook for copying the text inside my input;
   const passwordRef = useRef(null)
   // using useref Hook for copying the text inside my input; 
-  
+
 
   // creating Password genrator method; and using callback hook*******
   const passwordGenrator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWYZqwertyuioplkjhgfdsazxcvbnm";
-    if (isNum) str += "0123456789";
-    if (isChar) str += "!@#$%^&*";
+    if (isNum) {
+      str += "0123456789"
+    } else if (isChar) {
+      str += "!@#$%^&*_+"
+    }
 
     // from this loop we will get a index of str;
     for (let i = 1; i <= length; i++) {
@@ -32,35 +35,40 @@ function App() {
   }, [length, isNum, isChar])
 
   // onclick function for copy button;
-  const copyPassToClipBord = useCallback(() => {passwordRef.current?.select() , window.navigator.clipboard.writeText(password)} , [password])
+  const copyPassToClipBord = useCallback(() => { passwordRef.current?.select(), 
+    window.navigator.clipboard.writeText(password) },
+    [password])
 
   // using use effect hook for running password grnrator function***********
-  useEffect(()=>{
+  useEffect(() => {
     passwordGenrator()
-  } , [length, isNum, isChar, passwordGenrator])
+  }, [length, isNum, isChar])
   // using useeffect hook***********
 
   return (
     <>
-      <h1>Pass-Word Genrator 😍</h1>
-      <div>
-        <input className='input-1' type="text" value={password} placeholder='Password' readOnly ref={passwordRef}/>
-        <button className='input-1 btn' onClick={copyPassToClipBord}>Copy</button>
-      </div>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg p-4 m-8 text-orange-500 bg-gray-800'>
+        <h1 className='text-4xl text-center m-4 p-4 rounded-lg'>Pass-Word Genrator 😍</h1>
+        <div>
+          <input className='input-1' type="text" value={password} placeholder='Password' readOnly ref={passwordRef} />
+          <button className='input-1 btn' onClick={copyPassToClipBord}>Copy</button>
+        </div>
 
-      <div>
-        <input className='input-1 ptr' type="range" onChange={(e) => { SetLenght(e.target.value) }} min={8} max={16} value={length} />
-        <label htmlFor="">Lenght: {length}</label>
-      </div>
+        
+        <div>
+          <input className='cursor-pointer' type="range" onChange={(e) => { SetLenght(e.target.value) }} min={8} max={60} value={length} />
+          <label htmlFor="">Lenght: {length}</label>
+        </div>
 
-      <div>
-        <input type="checkbox" defaultChecked = {isNum} id='numInput' onChange={() => {setIsnum((prev) => !prev)}}/>
-        <label htmlFor="">Numbers</label>
-      </div>
+        <div>
+          <input type="checkbox" defaultChecked={isNum} id='numInput' onChange={() => { setIsnum((prev) => !prev) }} />
+          <label htmlFor="">Numbers</label>
+        </div>
 
-      <div>
-        <input type="checkbox" defaultChecked = {isChar} id='charInput' onChange={() => {setIschar((prev) => !prev)}}/>
-        <label htmlFor="">Charecters</label>
+        <div>
+          <input type="checkbox" defaultChecked={isChar} id='charInput' onChange={() => { setIschar((prev) => !prev) }} />
+          <label htmlFor="">Charecters</label>
+        </div>
       </div>
     </>
   )
